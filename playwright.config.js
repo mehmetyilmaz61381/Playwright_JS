c776f458d6b50e76a0b2e5e0ff9b1856d7c4e6e6
mehmetyilmaz61381 <mehmetyilmaz61381@gmail.com>
2025-10-16 16:47:52 +0200

    html reporter is added

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'], // Konsola normal test çıktısı
-    ['allure-playwright'] // Allure raporu üretir
+    ['allure-playwright'], // Allure raporu üretir
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
